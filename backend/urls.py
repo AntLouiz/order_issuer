9399d23e69be902130a2d0850095aacf3114f587
AntLouiz <luizrodrigo46@hotmail.com>
2021-03-02 15:10:52 -0300

    First commit

---
+++ backend/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    # Django admin
+    path('admin/', admin.site.urls),
+    # Local apps
+    path('', include('backend.core.urls', namespace='core')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
