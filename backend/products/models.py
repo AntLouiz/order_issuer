33e88982cb5a1b9e1fbbdd487ef2beb2f72c8ae5
AntLouiz <luizrodrigo46@hotmail.com>
2021-03-07 17:33:25 -0300

    Add order model to products app

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from backend.clients.models import Client
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     multiple = models.IntegerField(blank=True, null=True)
+
+
+class Order(models.Model):
+    products = models.ManyToManyField(Product)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
---
+++ backend/products/tests/test_order_model.py
@@ -0,0 +1,40 @@
+import pytest
+from backend.products.models import Product, Order
+from backend.products.tests.schemas import ProductSchema
+from backend.clients.models import Client
+from backend.clients.tests.schemas import ClientSchema
+
+
+def test_instance():
+    order = Order()
+    assert isinstance(order, Order)
+
+
+@pytest.fixture
+def products_list(db):
+    products = [Product.objects.create(name='R2d2', price=100),
+               Product.objects.create(name='C3PO', price=200)]
+
+    return products
+
+@pytest.fixture
+def client(db):
+    client = Client.objects.create(name='R2d2')
+    return client
+
+
+def test_instance_fields(products_list, client, db):
+    expected_fields = ['id',
+                       'client_id',
+                       'created_at',
+                       'updated_at',
+                       '_state']
+
+    order_dict = {"client": client}
+    order = Order.objects.create(client=client)
+    for product in products_list:
+        order.products.add(product)
+
+    object_fields = order.__dict__.keys()
+
+    assert set(object_fields) == set(expected_fields)
