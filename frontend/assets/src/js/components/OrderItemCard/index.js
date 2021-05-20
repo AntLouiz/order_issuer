17e63db67c0f1a0746ccf0fc460ef2aebc6d4f1a
AntLouiz <luizrodrigo46@hotmail.com>
2021-05-20 20:04:44 -0300

    Add remove button

--- frontend/assets/src/js/components/OrderItemCard/index.js
+++ frontend/assets/src/js/components/OrderItemCard/index.js
@@ -4,6 +4,8 @@ import Card from '@material-ui/core/Card';
 import OrderItemModal from '../../components/OrderItemModal'
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
+import { Button } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid'
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -23,7 +25,8 @@ const useStyles = makeStyles({
     margin: "auto"
   },
   actions: {
-    display: "inline"
+    display: "inline",
+    width: "100%"
   },
   details: {
     alignItems: "center",
@@ -44,6 +47,10 @@ export default function OrderItemCard(props) {
     order['image_url'] = order.productItem.image_url
     order['description'] = order.productItem.description
     order['multiple'] = order.productItem.multiple
+
+    const handleRemove = () => {
+      console.log("Remove item")
+    }
 
     return (
       <Card className={classes.root}>
@@ -69,8 +76,22 @@ export default function OrderItemCard(props) {
               </Typography>
             </Typography>
           </CardContent>
-          <OrderItemModal className={classes.actions} order={order} setAppState={props.setAppState} appState={props.appState} modalActionText={"Editar item"} isEdition={true}/>
-        </CardActionArea>
+          <Grid container xs={12}>
+            <Grid item xs={6}>
+              <OrderItemModal className={classes.actions} order={order} setAppState={props.setAppState} appState={props.appState} modalActionText={"Editar item"} isEdition={true}/>
+            </Grid>
+            <Grid item xs={6}>
+              <Button
+                  variant="contained"
+                  color="secondary"
+                  className={classes.actions}
+                  onClick={handleRemove}
+              >
+                  Remover
+              </Button>
+            </Grid>
+          </Grid>
+          </CardActionArea>
       </Card>
     )
 }
