507e7658485768a24b8fafc4538fbadf35ac6090
AntLouiz <luizrodrigo46@hotmail.com>
2021-05-26 20:41:20 -0300

    Fix urls pep8

--- backend/orders/api/urls.py
+++ backend/orders/api/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 from . import views
 
 app_name = 'orders'
 
 urlpatterns = [
-    path('clients/<int:client_pk>/', views.ClientOrdersListView.as_view(), name='client-orders'),
-    path('clients/<int:client_pk>/current/', views.CurrentClientOrderAPIView.as_view(), name='current'),
+    path('clients/<int:client_pk>/',
+         views.ClientOrdersListView.as_view(),
+         name='client-orders'),
+    path('clients/<int:client_pk>/current/',
+         views.CurrentClientOrderAPIView.as_view(),
+         name='current'),
 ]
